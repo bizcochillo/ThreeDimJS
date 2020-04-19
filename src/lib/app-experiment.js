796234683a1a3d2ef6e21244f54fa17b7a3da593
abel <abelardop@gmail.com>
2020-04-20 00:37:23 +0200

    Moved functionality for initialization out of the class

--- src/lib/app-experiment.js
+++ src/lib/app-experiment.js
@@ -38,44 +38,30 @@ class Circuit {
     }
 }
 
-class CircuitHandler {
-    initialize() {
-        // fill with the two patterns.
-        let pattern1 = new Circuit(
-            'M -5 -60 1 E,F 10,H 15 15,F 10,L,F 10,H 15 -15,F 10,V 15 30,F 20,V 15 -30,F 10,H 15 15,F 10,L,F 10,H 15 -15,F 20,H 15 15,F 10,L,F 10,H 15 -15,F 10,V 15 30,F 20,V 15 -30,F 10,H 15 15,F 10,L,F 10,H 15 -15,F 10',
-            true);
-        pattern1.name = 'Example 1';
-        this.circuits.push(pattern1);
-        addCircuit(pattern1); // TODO: Remove UI interaction
+class CircuitHandler{
+  setCircuit(circuit)
+  {
+    this.circuits.push(circuit);
+  }
 
-        let pattern2 = new Circuit(
-            'M -30 0 4 E,F 30,V 10 13,L,F 7,V 6 -27,F 5,R,F 4,L,F 29,R,F 16,R,F 40,V 15 25,F 23,R,F 23,L,F 13,R,V 6 -80,F 32,H 17 -23,F 7,R,F 9,L,F 25,R,F 13,R,F 20,M -47 -6 10 E    ,F 57,M -49 -6 4 E,F 60,M -34 -8.5 4 S,V 7 -15,M -21 -8.5 4 S,V 7 -15,M -3 -8.5 4 S,V 7 -15,M 6 -8.5 4 S,V 7 -15,M -34 -8.5 10 S,V 7 -15,M -21 -8.5 10 S,V 7 -15,M -3 -8.5 10 S,V 7 -15,M 6 -8.5 10 S,V 7 -15',
-            true);
-        pattern2.name = 'Example 2';
-        this.circuits.push(pattern2);
-        addCircuit(pattern2); // TODO: Remove UI interaction
-    }
+  getCircuit(index)
+  {
+    return this.circuits[index];
+  }
 
-    setCircuit(circuit) {
-        this.circuits.push(circuit);
-    }
+  removeCircuit(index)
+  {
+    this.circuits.splice(index, 1);
+  }
 
-    getCircuit(index) {
-        return this.circuits[index];
-    }
+  getNumCircuits()
+  {
+    return this.circuits.length;
+  }
 
-    removeCircuit(index) {
-        this.circuits.splice(index, 1);
-    }
-
-    getNumCircuits() {
-        return this.circuits.length;
-    }
-
-    constructor() {
-        this.circuits = [];
-        this.initialize();
-    }
+  constructor(){
+    this.circuits = [];
+  }
 }
 
 function index(el) {
@@ -116,7 +102,21 @@ function selectCircuitHandler() {
 
 window.onload = function() {
     circuitHandler = new CircuitHandler;
+    // Add pattern circuits.
+    // fill with the two patterns.
+    let pattern1 = new Circuit(
+      'M -5 -60 1 E,F 10,H 15 15,F 10,L,F 10,H 15 -15,F 10,V 15 30,F 20,V 15 -30,F 10,H 15 15,F 10,L,F 10,H 15 -15,F 20,H 15 15,F 10,L,F 10,H 15 -15,F 10,V 15 30,F 20,V 15 -30,F 10,H 15 15,F 10,L,F 10,H 15 -15,F 10',
+      true);
+    pattern1.name = 'Example 1';
+    circuitHandler.setCircuit(pattern1);
+    addCircuit(pattern1); // TODO: Remove UI interaction
 
+    let pattern2 = new Circuit(
+      'M -30 0 4 E,F 30,V 10 13,L,F 7,V 6 -27,F 5,R,F 4,L,F 29,R,F 16,R,F 40,V 15 25,F 23,R,F 23,L,F 13,R,V 6 -80,F 32,H 17 -23,F 7,R,F 9,L,F 25,R,F 13,R,F 20,M -47 -6 10 E    ,F 57,M -49 -6 4 E,F 60,M -34 -8.5 4 S,V 7 -15,M -21 -8.5 4 S,V 7 -15,M -3 -8.5 4 S,V 7 -15,M 6 -8.5 4 S,V 7 -15,M -34 -8.5 10 S,V 7 -15,M -21 -8.5 10 S,V 7 -15,M -3 -8.5 10 S,V 7 -15,M 6 -8.5 10 S,V 7 -15',
+      true);
+    pattern2.name = 'Example 2';
+    circuitHandler.setCircuit(pattern2);
+    addCircuit(pattern2);  // TODO: Remove UI interaction
     // Add active class to the current button (highlight it)
     var header = document.getElementsByClassName("picker")[0];
     var btns = header.getElementsByClassName("circuit-button");
